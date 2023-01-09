3897b1341def2860f3622da3790bdba1fd6f7205
GitHub Action <action@github.com>
2023-01-09 22:15:21 +0000

    Template cleanup

--- src/main/kotlin/com/github/rockjam/clojureinline/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/rockjam/clojureinline/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.rockjam.clojureinline.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.rockjam.clojureinline.MyBundle
 
 class MyApplicationService {
 
